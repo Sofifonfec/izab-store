7ba87f1ce350c14f03b9f0b92bbc828d6a834218
Sophie <soph.charrier@gmail.com>
2018-04-16 16:20:16 +0200

    Completed set up

--- gulpfile.js
+++ gulpfile.js
@@ -7,45 +7,38 @@ const cssImport = require('postcss-import');
 const mixins = require('postcss-mixins');  
 const hexrgba = require('postcss-hexrgba'); 
 const modernizr = require('gulp-modernizr');
-const webpack = require('webpack');
 const watch = require('gulp-watch');
 const browserSync = require('browser-sync').create();
 
 // STYLES TASK POSTCSS SET UP
 
 gulp.task('styles', function(){
-	return gulp.src('src/styles/styles.css')
+	return gulp.src('./App/src/styles/styles.css')
 		.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 		.on('error', function(errorInfo) {
 			console.log(errorInfo.toString());
 			this.emit('end');
 		})
-		.pipe(gulp.dest('build/styles'));
+		.pipe(gulp.dest('./App/build/styles'));
 });
 
 // MODERNIZR TASK
 
 gulp.task('modernizr', function() {
-	return gulp.src(['./src/styles/**/*.css', './src/scripts/**/*.js'])
+	return gulp.src(['./App/src/scripts/**/*.js'])
 		.pipe(modernizr({
 			"options": [
 				"setClasses"
 			]
 		}))
-		.pipe(gulp.dest('./build/scripts/'));
+		.pipe(gulp.dest('./App/build/scripts/'));
 });
 
 // SCRIPTS TASK USING WEBPACK ! HAVE TO CREATE NEW FILE IN ROOT
 
-gulp.task('scripts', ['modernizr'], function(callback) {
-	webpack(require('./webpack.config.js'), function(err, stats) {
-		if(err) {
-			console.log(err.toString());
-		}
-
-		console.log(stats.toString());
-		callback();
-	});
+gulp.task('scripts', ['modernizr'], function() {
+	return gulp.src('./App/src/scripts/*.js')
+		.pipe(gulp.dest('./App/build/scripts'));
 });
 
 // WATCH AND BROWSER SYNC
@@ -55,25 +48,25 @@ gulp.task('watch', function() {
 	browserSync.init({
 		notify: false,
 		server: {
-			baseDir: 'src'
+			baseDir: 'App'
 		}
 	});
 
-	watch('./build/index.html', function() {
+	watch('./App/index.html', function() {
 		browserSync.reload();
 	});
 
-	watch('./src/styles/**/*.css', function() {
+	watch('./App/src/styles/styles.css', function() {
 		gulp.start('cssInject');
 	});
 
-	watch('./src/scripts/**/*.js', function() {
+	watch('./App/src/scripts/**/*.js', function() {
 		gulp.start('scriptsRefresh');
 	});
 });
 
-gulp.task('cssInject', ['styles'] function() {
-	return gulp.src('./buil/styles/styles.css')
+gulp.task('cssInject', ['styles'], function() {
+	return gulp.src('./App/build/styles/styles.css')
 		.pipe(browserSync.stream());
 });
 
